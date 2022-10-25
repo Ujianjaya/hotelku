97ebdb8d66f746450b38a7f50387b38e3fd4a7cb
Ujianjaya <bumihayu01@gmail.com>
2022-10-25 02:07:16 -0800

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>   
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Putra </li>
+        <li>Nama Belakang : Ujianjaya</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Malangan, sentolo, kulon progo, Yogyakarta</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kulon Progo</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Sepeda</li>
+        <li>Futsal</li>
+    </ul>
+</section>
